30951851a0210a196a6fddcea67ec7067bae2529
Leonardo Ramos <leoramos@everis.com>
2019-09-04 14:22:28 -0300

    Identação estava quebrada

--- src/main/java/me/lramos/consumer/moedas/dto/MoedaDTO.java
+++ src/main/java/me/lramos/consumer/moedas/dto/MoedaDTO.java
@@ -52,49 +52,49 @@ import me.lramos.consumer.moedas.json.TimestampDeserializer;
 @Data
 public class MoedaDTO {
 
-    @Setter(onMethod_ = { @JsonSetter("code") })
-    @Getter(onMethod_ = { @JsonGetter("codigo") })
+	@Setter(onMethod_ = { @JsonSetter("code") })
+	@Getter(onMethod_ = { @JsonGetter("codigo") })
 	private String codigo;
 
-    @Setter(onMethod_ = { @JsonSetter("codein") })
-    @Getter(onMethod_ = { @JsonGetter("codigoDestino") })
+	@Setter(onMethod_ = { @JsonSetter("codein") })
+	@Getter(onMethod_ = { @JsonGetter("codigoDestino") })
 	private String codigoDestino;
 
-    @Setter(onMethod_ = { @JsonSetter("name") })
-    @Getter(onMethod_ = { @JsonGetter("nome") })
+	@Setter(onMethod_ = { @JsonSetter("name") })
+	@Getter(onMethod_ = { @JsonGetter("nome") })
 	private String nome;
 
-    @Setter(onMethod_ = { @JsonSetter("high") })
-    @Getter(onMethod_ = { @JsonGetter("alta") })
+	@Setter(onMethod_ = { @JsonSetter("high") })
+	@Getter(onMethod_ = { @JsonGetter("alta") })
 	private BigDecimal alta;
 
-    @Setter(onMethod_ = { @JsonSetter("low") })
-    @Getter(onMethod_ = { @JsonGetter("baixa") })
+	@Setter(onMethod_ = { @JsonSetter("low") })
+	@Getter(onMethod_ = { @JsonGetter("baixa") })
 	private BigDecimal baixa;
 
-    @Setter(onMethod_ = { @JsonSetter("varBid") })
-    @Getter(onMethod_ = { @JsonGetter("variacao") })
+	@Setter(onMethod_ = { @JsonSetter("varBid") })
+	@Getter(onMethod_ = { @JsonGetter("variacao") })
 	private BigDecimal variacao;
 
-    @Setter(onMethod_ = { @JsonSetter("pctChange") })
-    @Getter(onMethod_ = { @JsonGetter("porcentagemDeVariacao") })
+	@Setter(onMethod_ = { @JsonSetter("pctChange") })
+	@Getter(onMethod_ = { @JsonGetter("porcentagemDeVariacao") })
 	private BigDecimal porcentagemDeVariacao;
 
-    @Setter(onMethod_ = { @JsonSetter("bid") })
-    @Getter(onMethod_ = { @JsonGetter("compra") })
+	@Setter(onMethod_ = { @JsonSetter("bid") })
+	@Getter(onMethod_ = { @JsonGetter("compra") })
 	private BigDecimal compra;
 
-    @Setter(onMethod_ = { @JsonSetter("ask") })
-    @Getter(onMethod_ = { @JsonGetter("venda") })
+	@Setter(onMethod_ = { @JsonSetter("ask") })
+	@Getter(onMethod_ = { @JsonGetter("venda") })
 	private BigDecimal venda;
 
-    @JsonProperty
+	@JsonProperty
 	@JsonDeserialize(using = TimestampDeserializer.class)
 	@JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
 	private String timestamp;
 
-    @Setter(onMethod_ = { @JsonSetter("create_date") })
-    @Getter(onMethod_ = { @JsonGetter("dataConsulta") })
+	@Setter(onMethod_ = { @JsonSetter("create_date") })
+	@Getter(onMethod_ = { @JsonGetter("dataConsulta") })
 	private String data;
 
 }
